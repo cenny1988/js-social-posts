9f5b0e46cfd59e634db8ba988a6fb3517c645d06
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2021-11-05 15:30:32 +0100

    creato ciclo for per inserire tutti i post dellla lista di obj

---
+++ main.js
@@ -0,0 +1,95 @@
+/*
+Ricreiamo un feed social aggiungendo al layout di base fornito, il nostro javascript in cui:
+- Creiamo il nostro array di oggetti che rappresentano ciascun post.
+Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+nome autore,
+foto profilo,
+data,
+testo del post,
+immagine (non tutti i post devono avere una immagine) [quindi gestisco il caso],
+numero di likes.
+- Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
+- Rendiamo il tasto “Mi Piace” cliccabile con incremento del counter dei likes.
+Consigli del giorno:
+Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
+*/
+
+//Creiamo il nostro array di oggetti che rappresentano ciascun post.
+
+const posts = [
+    {
+        "imgProfile": 'https://picsum.photos/60?random=1',
+        "author": 'Fusco Gennaro',
+        "date": '4 mesi fa',
+        "postText": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet eleifend orci, sit amet pretium orci. Integer non velit ut leo condimentum cursus quis quis arcu. Vestibulum porttitor a sem quis semper. Ut at metus at ex molestie rhoncus. Donec lectus nulla, fermentum eu gravida quis, iaculis ac dolor.',
+        "imgPost": 'https://unsplash.it/600/300?random=1',
+        "likes": 80
+    },
+    {
+        "imgProfile": 'https://picsum.photos/60?random=2',
+        "author": 'Valentina',
+        "date": '6 mesi fa',
+        "postText": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent mauris magna, venenatis in sollicitudin eget, laoreet in nunc. Cras orci diam, tincidunt vitae neque suscipit, fermentum tincidunt lectus. Nulla a lectus in leo aliquam sodales. Mauris nec nibh vitae risus vulputate tempus pretium vitae enim. Aliquam sit amet viverra neque.',
+        "imgPost": 'https://unsplash.it/600/300?random=2',
+        "likes": 24
+    },
+    {
+        "imgProfile": 'https://picsum.photos/60?random=3',
+        "author": 'Marco',
+        "date": '1 mesi fa',
+        "postText": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur purus enim, volutpat in metus at, viverra cursus neque. Sed vel justo sed nibh pellentesque congue. Praesent imperdiet tellus quis dui facilisis vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        "imgPost": '',//https://unsplash.it/600/300?random=3
+        "likes": 55
+    }
+];
+
+
+// stampiamo i post del nostro feed. --> proviamo prima 1 stampa singola.
+const containerPosts = document.getElementById('container');
+
+//creamo un for lungo come tutti i posts così da inserirli tutti nella pagina!
+for (i=0; i<posts.length; i++){
+    //creamo un nuovo div e aggiungiamo la classe post
+    const divPost = document.createElement('div');
+    divPost.classList.add('post');
+    
+    //creo un obj e lo destrutturo così da poter copiare i valori delle prop in newPost!
+    const {imgProfile, author, date, postText, imgPost, likes} = posts[i];
+    
+    //poi al suo interno tramite innerHTML copiamo tutta la struttura
+    let newPost = '';
+    newPost = `
+                <div class="post__header">
+                    <div class="post-meta">                    
+                        <div class="post-meta__icon">
+                            <img class="profile-pic" src="${imgProfile}" alt="${author}">                    
+                        </div>
+                        <div class="post-meta__data">
+                            <div class="post-meta__author">${author}</div>
+                            <div class="post-meta__time">${date}</div>
+                        </div>                    
+                    </div>
+                </div>
+                <div class="post__text">${postText}</div>
+                <div class="post__image">
+                    <img src="${imgPost}" alt="">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button  js-like-button" href="#" data-postid="1">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                        </div>
+                    </div> 
+                </div>            
+    `;
+    // infine copiamo il contenuto nella pagina
+    divPost.innerHTML = newPost;
+    containerPosts.append(divPost);
+}
+
